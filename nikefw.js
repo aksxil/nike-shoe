cf024f1eb4fdfe2c4f3436a83fb64f4462281117
aksxil <111107009+aksxil@users.noreply.github.com>
2023-05-10 18:59:20 +0530

    Add files via upload

---
+++ nikefw.js
@@ -0,0 +1,39 @@
+gsap.from(".main",{
+    delay:.4,
+    opacity:0,
+    duration:.5,
+    y:-100
+})
+gsap.from(".center",{
+    delay:2,
+    opacity:0,
+    duration:1,
+    x:-100
+})
+gsap.from(".right , .left",{
+    delay:2,
+    opacity:0,
+    duration:1,
+    y:100
+})
+gsap.from(".nav",{
+    delay:2,
+    opacity:0,
+    duration:1,
+    y:-100
+})
+gsap.from(".main img",{
+    delay:2,
+    opacity:0,
+    duration:1,
+    x:-100,
+    scale:2
+})
+gsap.from(".page2",{
+    delay:3,
+    opacity:0,
+    duration:.5,
+    y:-100
+})
+
+
